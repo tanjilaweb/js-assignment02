5303d30cf08fcc2e7834b3ccb4f60cb1aae57e58
tanjilaweb <154679873+tanjilaweb@users.noreply.github.com>
2024-02-19 14:52:52 +0600

    Add files via upload

---
+++ scrive.js
@@ -0,0 +1,27 @@
+
+const cars=["saab","volvo","BMw"];
+cars.push("Tanjila")
+cars.pop()
+
+console.log(cars);
+
+
+
+
+let makapprice = 10000;
+
+let myBudget = 3000;
+
+if(makapprice > myBudget){
+
+    console.log("Give me blation");
+}else{
+    console.log("Give me makap")
+}
+
+Var person={
+    fistname: "Tanjila",
+    lastname: "Islam",
+    eyecolor:"blue"
+};
+console.log(person.lenght);
